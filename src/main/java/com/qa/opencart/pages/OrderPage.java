826fba829f338403edb1a5334a7a1221eabf6d4e
SunilLearning <sunillearning96@gmail.com>
2023-05-26 21:38:45 +0530

    OrderPage is added in Pages component

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+  
+public class OrderPage {
+	private By username= By.xpath("name");
+	
+	public OrderPage(WebDriver driver)
+	{
+		System.out.println("hello");
+	}
+
+}
